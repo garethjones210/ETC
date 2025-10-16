3a51a1067808d9dee22c3b570461ca6721573e19
garethjones210 <garethjones2109@gmail.com>
2025-10-16 17:08:21 -0500

    version of testing the first bpass spectrum run

--- _version.py
+++ _version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev322+g2f6d351da.d20251015'
-__version_tuple__ = version_tuple = (0, 1, 'dev322', 'g2f6d351da.d20251015')
+__version__ = version = '0.1.dev325+g951b4a47f.d20251016'
+__version_tuple__ = version_tuple = (0, 1, 'dev325', 'g951b4a47f.d20251016')
 
 __commit_id__ = commit_id = None
