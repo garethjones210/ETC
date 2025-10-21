1e4f934b2086846d30ff37f5057fe785ec6f83d4
garethjones210 <garethjones2109@gmail.com>
2025-10-21 16:44:23 -0500

    working version with redshift and luminoisty distance correction

--- _version.py
+++ _version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev328+g1bdff3d90.d20251018'
-__version_tuple__ = version_tuple = (0, 1, 'dev328', 'g1bdff3d90.d20251018')
+__version__ = version = '1.3.5.dev59+gc1385497b.d20251020'
+__version_tuple__ = version_tuple = (1, 3, 5, 'dev59', 'gc1385497b.d20251020')
 
 __commit_id__ = commit_id = None
