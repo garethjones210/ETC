963b3c9736f5e4c1173b3f9e6625590af0a11581
garethjones210 <garethjones2109@gmail.com>
2025-11-25 14:27:11 -0600

    version with nebular emission model

--- _version.py
+++ _version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.3.5.dev86+g5369eb1a3.d20251104'
-__version_tuple__ = version_tuple = (1, 3, 5, 'dev86', 'g5369eb1a3.d20251104')
+__version__ = version = '1.3.5.dev95+g02a933a65.d20251125'
+__version_tuple__ = version_tuple = (1, 3, 5, 'dev95', 'g02a933a65.d20251125')
 
 __commit_id__ = commit_id = None
